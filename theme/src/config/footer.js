406a2ef1ce0d3c118342a65c9f5ebefd0f838305
RyanDrx <Ryan.Dierickx@gmail.com>
2023-02-05 19:42:12 -0800

    lock down extra content

--- theme/src/config/footer.js
+++ theme/src/config/footer.js
@@ -1,80 +1,75 @@
 export const footerSocials = [
-  {
-    name: 'Twitter',
-    url: 'https://twitter.com/littlesticksdev',
-    icon: "mdi:twitter",
-  },
-  {
-    name: 'Youtube',
-    url: 'https://youtube.com/c/LittleSticks',
-    icon: "mdi:youtube",
-  },
-  {
-    name: 'Github',
-    url: 'https://github.com/littlesticks',
-    icon: "mdi:github",
-  },
-  {
-    name: 'Discord',
-    url: 'https://littlesticks.dev/discord',
-    icon: "ic:baseline-discord",
-  },
-]
+    {
+        name: 'Twitter',
+        url: 'https://twitter.com/',
+        icon: "mdi:twitter",
+    },
+    {
+        name: 'Youtube',
+        url: 'https://youtube.com/c/',
+        icon: "mdi:youtube",
+    },
+    {
+        name: 'Github',
+        url: 'https://github.com/',
+        icon: "mdi:github",
+    },
+];
 
 export const footerLists = [
-	{
-		title: 'Landing Pages',
-		items: [
-			{
-				title: 'Landing Page 1',
-				slug: '/landing-pages/landing-1',
-			},
-			{
-				title: 'Landing Page 2',
-				slug: '/landing-pages/landing-2',
-			},
-      // {
-			// 	title: 'Landing Page 3',
-			// 	slug: '/landing-pages/landing-3',
-			// },
-		],
-	},
+	// {
+	// 	// title: 'Landing Pages',
+	// 	// items: [
+	// 	// 	{
+	// 	// 		title: 'Landing Page 1',
+	// 	// 		slug: '/landing-pages/landing-1',
+	// 	// 	},
+	// 	// 	{
+	// 	// 		title: 'Landing Page 2',
+	// 	// 		slug: '/landing-pages/landing-2',
+	// 	// 	},
+    //   // {
+	// 		// 	title: 'Landing Page 3',
+	// 		// 	slug: '/landing-pages/landing-3',
+	// 		// },
+	// 	],
+	// },
 	{
 		title: 'Company',
 		items: [
 			{
 				title: 'About',
-				slug: '/company/about',
+				slug: '/',
 			},
 			{
 				title: 'Blog',
-				slug: '/blog',
+				slug: '/',
 			},
 			{
 				title: 'Contact',
-				slug: '/company/contact',
+				slug: '/',
 			},
 		],
 	},
-	{
-		title: 'Theme',
-		items: [
-			{
-				title: 'Get Started',
-				slug: '/theme/get-started',
-			},
-			{
-				title: 'Style Guide',
-				slug: '/theme/style-guide',
-			},
-      {
-        title: 'Theme Setup',
-        slug: '/theme/theme-setup',
-      },
-      {
-        title: 'Customizing Odyssey',
-        slug: '/theme/customizing-odyssey',
-      }
-		],
-	},
+	// {
+	// 	title: 'Theme',
+	// 	items: [
+	// 		{
+	// 			title: 'Get Started',
+	// 			slug: '/theme/get-started',
+	// 		},
+	// 		{
+	// 			title: 'Style Guide',
+	// 			slug: '/theme/style-guide',
+	// 		},
+    //   {
+    //     title: 'Theme Setup',
+    //     slug: '/theme/theme-setup',
+    //   },
+    //   {
+    //     title: 'Customizing Odyssey',
+    //     slug: '/theme/customizing-odyssey',
+    //   }
+	// 	],
+	// },
 ];
--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -2,7 +2,7 @@ export default {
 	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
 	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
 	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-  enableThemeSwitcher: true,
-  showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+	name: `Empowered Healing Therapy`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+  enableThemeSwitcher: false,
+  showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
