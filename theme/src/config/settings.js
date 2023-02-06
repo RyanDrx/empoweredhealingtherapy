67b6a0bc1dc91105685aaddc8433c9d1642266ce
RyanDrx <Ryan.Dierickx@gmail.com>
2023-02-05 19:49:21 -0800

    update settings.js

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
+	title: `Empowered Healing Therapy`,
+	description: `Discover a path to wellness with Empowered Healing Therapy. I'm an experienced therapist specialize in helping individuals overcome challenges and lead happier, healthier lives through somatic experiencing, EMDR, and other therapeutic modalities.`,
+	url: `NA`, // No trailing slash!
 	name: `Empowered Healing Therapy`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-  enableThemeSwitcher: false,
-  showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+	enableThemeSwitcher: true,
+	showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
