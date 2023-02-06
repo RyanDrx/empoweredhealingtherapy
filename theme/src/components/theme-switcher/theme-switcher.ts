78853212ce666d3afc7a0195f0c87b208645c868
RyanDrx <Ryan.Dierickx@gmail.com>
2023-02-05 18:56:01 -0800

    update home page and trim theme files not in use

--- theme/src/components/theme-switcher/theme-switcher.ts
+++ theme/src/components/theme-switcher/theme-switcher.ts
@@ -104,19 +104,19 @@ export class ThemeSwitcher extends LitElement {
 
     const _heroImage = document.querySelector('#home-hero-image') as HTMLImageElement;
 		if (theme === 'default') {
-			_heroImage.src = '/assets/images/home/classic-hero.jpg';
+			_heroImage.src = '/assets/images/home/classic-hero.png';
 		}
 		if (theme === 'dark') {
-			_heroImage.src = '/assets/images/home/dark-hero.jpg';
+            _heroImage.src = '/assets/images/home/classic-hero.png';
 		}
 		if (theme === 'earth') {
-			_heroImage.src = '/assets/images/home/earth-hero.jpg';
+            _heroImage.src = '/assets/images/home/classic-hero.png';
 		}
 		if (theme === 'ocean') {
-			_heroImage.src = '/assets/images/home/ocean-hero.jpg';
+            _heroImage.src = '/assets/images/home/classic-hero.png';
 		}
 		if (theme === 'sand') {
-			_heroImage.src = '/assets/images/home/sand-hero.jpg';
+            _heroImage.src = '/assets/images/home/classic-hero.png';
 		}
 		localStorage.setItem('theme', theme);
 		this.theme = theme;
