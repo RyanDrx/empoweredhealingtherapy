83276c513a10a98294be42a53a715bcb3f90ee3a
RyanDierickx <rndrx2@gmail.com>
2023-02-06 19:33:22 -0800

    Inline components

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `Empowered Healing Therapy`,
+	title: `Empowered Healing Counseling`,
 	description: `Discover a path to wellness with Empowered Healing Therapy. I'm an experienced therapist specialize in helping individuals overcome challenges and lead happier, healthier lives through somatic experiencing, EMDR, and other therapeutic modalities.`,
 	url: `NA`, // No trailing slash!
-	name: `Empowered Healing Therapy`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	name: `Empowered Healing Counseling`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: true,
 	showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
