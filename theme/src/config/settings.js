b4226cf242fb5d3a2e4c65822149836e50658d71
RyanDierickx <rndrx2@gmail.com>
2023-02-20 09:43:16 -0800

    seo

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
 	title: `Empowered Healing Counseling`,
-	description: `Discover a path to wellness with Empowered Healing Therapy. I'm an experienced therapist specialize in helping individuals overcome challenges and lead happier, healthier lives through somatic experiencing, EMDR, and other therapeutic modalities.`,
+	description: `Discover a path to wellness with Empowered Healing Counseling. I'm an experienced therapist specializing in helping individuals overcome challenges and lead happier, healthier lives through somatic experiencing. Focusing on trauma, anxiety, depression, and more, I offer a safe, supportive environment for you to explore your emotions and find the tools you need to heal.`,
 	url: `NA`, // No trailing slash!
 	name: `Empowered Healing Counseling`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
