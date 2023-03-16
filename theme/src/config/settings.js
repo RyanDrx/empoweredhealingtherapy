a0c436b05c5e6447ca0f6faea71485152246833f
RyanDierickx <rndrx2@gmail.com>
2023-03-15 15:29:08 -1000

    cleanup + social.png

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `Empowered Healing Counseling`,
-	description: `Discover a path to wellness with Empowered Healing Counseling. I'm an experienced therapist specializing in helping individuals overcome challenges and lead happier, healthier lives through somatic experiencing. Focusing on trauma, anxiety, depression, and more, I offer a safe, supportive environment for you to explore your emotions and find the tools you need to heal.`,
-	url: `NA`, // No trailing slash!
+    description: `Discover a path to wellness with Empowered Healing Counseling. I'm an experienced therapist specializing in helping individuals lead happier, healthier lives through somatic experiencing. Focusing on trauma, anxiety, depression, and more, I offer a safe, supportive environment for you to explore your emotions and find the tools you need to heal.`,
+	url: `EmpoweredHealingWa.com`, // No trailing slash!
 	name: `Empowered Healing Counseling`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
 	showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
