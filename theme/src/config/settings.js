86dff3a34c1cd4b946c4b355b804c7105c76d7da
RyanDierickx <rndrx2@gmail.com>
2023-03-23 21:21:38 -0700

    desc

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
 	title: `Empowered Healing Counseling | Meaghan Trezza, LMHC`,
-	description: `Discover a path to wellness with Empowered Healing Counseling. Meaghan Trezza, LMHC is an experienced telahealth therapist in Tacoma, Washington State specializing in helping individuals lead happier, healthier lives through somatic experiencing. Focusing on trauma, anxiety, depression, and more, I offer a safe, supportive environment for you to explore your emotions and find the tools you need to heal.`,
+	description: `Discover a path to wellness with Empowered Healing Counseling. Meaghan Trezza, LMHC is an experienced telehealth therapist in Tacoma, Washington. Focusing on trauma, anxiety, depression, somatic experiencing, and more. Schedule a free 15-minute consultation today!`,
 	url: `EmpoweredHealingWa.com`, // No trailing slash!
 	name: `Empowered Healing Counseling`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
