5aa08b846bf67f417541f3c4c38da99500639828
RyanDierickx <rndrx2@gmail.com>
2023-03-23 20:44:46 -0700

    seo & style guide

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
-	title: `Empowered Healing Counseling`,
-    description: `Discover a path to wellness with Empowered Healing Counseling. I'm an experienced therapist specializing in helping individuals lead happier, healthier lives through somatic experiencing. Focusing on trauma, anxiety, depression, and more, I offer a safe, supportive environment for you to explore your emotions and find the tools you need to heal.`,
+	title: `Empowered Healing Counseling | Meaghan Trezza, LMHC`,
+	description: `Discover a path to wellness with Empowered Healing Counseling. Meaghan Trezza, LMHC is an experienced telahealth therapist in Tacoma, Washington State specializing in helping individuals lead happier, healthier lives through somatic experiencing. Focusing on trauma, anxiety, depression, and more, I offer a safe, supportive environment for you to explore your emotions and find the tools you need to heal.`,
 	url: `EmpoweredHealingWa.com`, // No trailing slash!
 	name: `Empowered Healing Counseling`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
