86b067806b3e6f3751b4a8f4f5c5adf64341523d
RyanDrx <Ryan.Dierickx@gmail.com>
2025-07-02 04:18:06 +0000

    Update package-lock.json and remove sitemap generation from Astro configuration

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -10,13 +10,10 @@ import partytown from '@astrojs/partytown';
 export default defineConfig({
 	site: 'https://www.empoweredhealingwa.com',
 	// Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-	sitemap: true,
-	// Generate sitemap (set to "false" to disable)
 	integrations: [
 		sitemap(),
 		mdx(),
 		lit(),
-		//critters(),
 		robotsTxt({
 			sitemapBaseFileName: 'sitemap',
 		}),
