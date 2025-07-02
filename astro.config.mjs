448f2114c40751d5b0b8acd2ee3d4d2a34c90027
RyanDrx <Ryan.Dierickx@gmail.com>
2025-07-02 03:57:41 +0000

    Update Astro configuration and package dependencies; add Partytown integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify/functions";
+import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.empoweredhealingwa.com',
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  compressHTML: true,
+  integrations: [
+    sitemap(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ]
 });
